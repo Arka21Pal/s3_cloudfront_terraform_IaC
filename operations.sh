84a888d5a4b2eba5a928b30711e2d169a6e2f1ac
Arka21Pal <write2arkapal@gmail.com>
2023-11-24 17:10:55 -0500

    refactor: script for frequently-used terraform commands

--- operations.sh
+++ operations.sh
@@ -4,7 +4,7 @@
 s_function() {
     printf "You called the -s flag. This flag is used to set the variables TF_LOG and TF_LOG_PATH.\nTF_LOG will be set to TRACE\nTF_LOG_PATH will be set to /home/happy/repos/terraform-deployment/current-logs\n"
     export TF_LOG="TRACE"
-    export TF_LOG_PATH="/home/happy/repos/terraform-deployment/current-logs"
+    export TF_LOG_PATH=""
 }
 
 d_function() {
@@ -22,8 +22,18 @@ p_function() {
     terraform plan
 }
 
+D_function() {
+    printf "You called the -D flag. This flag is used to run 'terraform destroy -auto-approve'.\n"
+    terraform destroy -auto-approve
+}
+
+A_function() {
+    printf "You called the -A flag. This flag is used to run 'terraform apply -auto-approve'.\n"
+    terraform apply -auto-approve
+}
+
 h_function() {
-    printf "Help Information:\n%s\n%s\n%s\n%s\n%s" "-s: Set a variable" "-d: Run 'terraform destroy'" "-a: Run 'terraform apply'" "-p: Run 'terraform plan'" "-h: Display this help information"
+    printf "Help Information:\n%s\n%s\n%s\n%s\n%s\n%s\n%s" "-s: Set a variable" "-d: Run 'terraform destroy'" "-D: Run 'terraform destroy -auto-approve'" "-a: Run 'terraform apply'" "-A: Run 'terraform apply -auto-approve'" "-p: Run 'terraform plan'" "-h: Display this help information"
 }
 
 # Check if exactly one argument was passed
@@ -37,12 +47,14 @@ fi
 # Parse the flags
 # getopts is a utility to parse flags
 # $@ is a special variable in shell that holds all positional parameters as a list of words
-while getopts sdaph flag
+while getopts sdapADh flag
 do
     case "${flag}" in
-        s) s_function;;
+        *s) s_function;;
         d) d_function;;
+        D) D_function;;
         a) a_function;;
+        A) A_function;;
         p) p_function;;
         h) h_function;;
         *) printf "Invalid flag. Use -h for help.\n"; exit 1;;
