9f2cbe6db3e872ec3595a4382fcd10c0ad835944
Arka21Pal <write2arkapal@gmail.com>
2023-11-24 20:20:21 -0500

    chore: relevant information to the console

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,13 @@
-output "s3_website_endpoint" {
-  description = "The domain name of the S3 website endpoint"
-  value       = aws_s3_bucket_website_configuration.website_bucket_hosting_configuration.website_endpoint
+output "s3_IAM_policy_document" {
+    description = "IAM policy document stating access to S3"
+    value = data.aws_iam_policy_document.website_bucket_IAM_policy.json
 }
-
 output "cloudfront_domain_endpoint" {
   description = "The domain name of the CloudFront distribution"
   value       = aws_cloudfront_distribution.website_bucket_distribution.domain_name
+}
+
+output "cloudfront_domain_arn" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.website_bucket_distribution.arn
 }
